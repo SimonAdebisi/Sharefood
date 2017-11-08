6023035c8dcbb277f1026d1058aafe5fa63f6bb0
MSI\Student <malletsimon1@gmail.com>
2017-11-08 16:35:44 +0100

    Travail du 8 novembre

--- page accueil + about us/Sharefood2.js
+++ page accueil + about us/Sharefood2.js
@@ -4,8 +4,58 @@
 //     texte.addClass('showing');
 // });
 
+
 $(document).ready(function(){
+	let text = $("#text")
 	$('#navmenu').click(function(){
 		$(this).toggleClass('open');
 	});
-});
+		text.animate({opacity:1}, 1500)
+		
+	});
+	
+	$.fn.characterize = function (wrapper, options) {
+		var txt = this.text(),
+			self = this;
+	  
+		this.empty();
+		
+		wrapper = wrapper || '<span />';
+		options = options || {};
+	  
+		Array.prototype.forEach.call(txt, function (c) {
+		  options.text = c;
+		  self.append($(wrapper, options));
+		});
+	  };
+	  
+	  
+	  function animate () {
+		let become = $("#become");
+	  
+		become.characterize('<span />', {
+		  class: 'fd',
+		  css: {
+			opacity: 0
+		  }
+		});
+		
+		become.css('opacity', 1);
+	  
+		$('.fd').each(function (i) {
+		  $(this).animate({opacity: 1}, (i + 1) * 200);
+		});
+	  }
+	  
+	  
+	  animate();
+
+
+
+	let dropcont = $(".dropdown-content")
+	function dropdown(){
+		dropcont.toggleClass("dropdownopen")
+	}
+
+	  let dropbtn = $(".dropbtn")
+	  dropbtn.click(dropdown)
